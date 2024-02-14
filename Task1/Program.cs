b771dc5c324328499fafab7022782a4c975ef4f3
Serge Borisov <emp_t@yandex.ru>
2024-02-14 22:39:59 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0) {
+    System.Console.WriteLine("да");
+} else {
+    System.Console.WriteLine("нет");
+}
